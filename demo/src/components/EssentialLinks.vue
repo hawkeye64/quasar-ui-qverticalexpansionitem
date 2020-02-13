05038530c2e82d7f85d44f5111175065142d4bc8
Jeff Galbraith <jeff.galbraith@xendra.com>
2020-02-13 08:22:27 -0700

    chore(docs): update demo

--- demo/src/components/EssentialLinks.vue
+++ demo/src/components/EssentialLinks.vue
@@ -2,14 +2,14 @@
   <q-list class="menu">
     <q-item clickable to="/docs">
       <q-item-section avatar>
-        <q-icon name="calendar_today" />
+        <q-icon name="bolt" />
       </q-item-section>
       <q-item-section>
         <q-item-label>QVerticalExpansionItem docs</q-item-label>
         <q-item-label caption>Documentation</q-item-label>
       </q-item-section>
     </q-item>
-    <q-item clickable to="/demo">
+    <!-- <q-item clickable to="/demo">
       <q-item-section avatar>
         <q-icon name="calendar_today" />
       </q-item-section>
@@ -17,10 +17,10 @@
         <q-item-label>QVerticalExpansionItem demo</q-item-label>
         <q-item-label caption>Interactively play with properties</q-item-label>
       </q-item-section>
-    </q-item>
+    </q-item> -->
     <q-item clickable to="/examples">
       <q-item-section avatar>
-        <q-icon name="calendar_today" />
+        <q-icon name="bolt" />
       </q-item-section>
       <q-item-section>
         <q-item-label>QVerticalExpansionItem examples</q-item-label>
